8d77a1f171b79421cf6ab2e439d6931ee7aeb155
mohamedkhedr700 <M.khedr@rowaad.net>
2023-08-30 23:07:23 +0300

    Enhance namespace

--- src/Models/Action/Action.php
+++ src/Models/Action/Action.php
@@ -2,9 +2,9 @@
 
 namespace Raid\Core\Enum\Models\Action;
 
-use Raid\Core\Enum\Traits\Action\Enum\WithAuthAction;
-use Raid\Core\Enum\Traits\Action\Enum\WithCrudAction;
-use Raid\Core\Enum\Traits\Action\Enum\WithProfileAction;
+use Raid\Core\Enum\Traits\Action\WithAuthAction;
+use Raid\Core\Enum\Traits\Action\WithCrudAction;
+use Raid\Core\Enum\Traits\Action\WithProfileAction;
 use Raid\Core\Enum\Traits\Enum\ConstEnum;
 
 class Action
