315a1ddd1d50a03e08550363dc538900b0160061
mohamedkhedr700 <M.khedr@rowaad.net>
2023-09-02 02:55:18 +0300

    Enhance namespace

--- src/Models/Action/Action.php
+++ src/Models/Action/Action.php
@@ -2,15 +2,15 @@
 
 namespace Raid\Core\Enum\Models\Action;
 
+use Raid\Core\Enum\Models\Enum;
 use Raid\Core\Enum\Traits\Action\WithAuthAction;
 use Raid\Core\Enum\Traits\Action\WithCrudAction;
 use Raid\Core\Enum\Traits\Action\WithProfileAction;
 use Raid\Core\Enum\Traits\Enum\ConstEnum;
 
-class Action
+class Action extends Enum
 {
-    use ConstEnum,
-        WithAuthAction,
+    use WithAuthAction,
         WithCrudAction,
         WithProfileAction;
 }
---
+++ src/Models/Enum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Raid\Core\Enum\Models;
+
+use Raid\Core\Enum\Traits\Enum\ConstEnum;
+
+class Enum
+{
+    use ConstEnum;
+}
